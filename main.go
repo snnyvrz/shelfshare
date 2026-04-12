d68fb13b740acbc5d9875fb58ef0d09638b0e812
sinaniya <sinaniya@gmail.com>
2026-04-12 04:50:06 +0200

    change ssh keys permission on server

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "ping pong 1",
+			"message": "ping pong 2",
 		})
 	})
 	router.Run()
